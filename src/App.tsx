cf43ab8827208459baa30d8e07ae62b401fa5873
Love Asoh <loveasoh@gmail.com>
2026-03-27 14:17:18 +0100

    Initialize Profile finder app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <div>
+        <h1 className="font-bold text-3xl p-4 flex justify-center">GitHub Profile Finder</h1>
+    </div>
+  )
+}
+
+export default App
